191eb5de0b09126e54d8e1b5fb590830c3045070
pcancio <pcancio@gmail.com>
2021-12-07 13:31:49 -0500

    fixed 11.1.8

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const { animals } = require('./data/animals');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-// app.get('/', (req, res) => {
-//     console.log('string');
-//     res.send('<h1>Hi there!</h1>');
-// })
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
@@ -40,6 +37,15 @@ function findbyId(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+
+app.get('api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findbyId(req.params.id, animals);
     if (result) {
@@ -49,11 +55,7 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
-// let results = animals; 
-// if (req.query) {
-//     results = filterByQuery(req.query, results);
-// }
-// res.json(results);
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
